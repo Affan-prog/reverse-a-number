42bf379e6ec8cd52fa149e4dddeb37149d75d18c
Affan-prog <122531527+Affan-prog@users.noreply.github.com>
2024-03-29 21:42:56 +0500

    Add files via upload

---
+++ program to reverse a number .py
@@ -0,0 +1,46 @@
+
+
+num = int(input("Enter number :"))
+
+str_num = str(num)
+len_num = len(str_num)
+
+
+# 1st method i.e By Converting number into string
+
+reversed_number = str_num[::-1]
+
+print("Reversed number :",reversed_number)
+
+
+
+
+# 2nd method i.e Using list , then iterating it
+
+# lst = []
+
+# for i in range(1,len_num+1):
+#     last_digit = num%10
+#     lst.append(last_digit)
+    
+#     new_num = num//10
+#     num = new_num
+
+
+# print("Reversed number:")
+
+# for reversed_num in lst:
+#     print(reversed_num, end='')
+
+
+
+
+
+
+
+
+
+
+
+
+
