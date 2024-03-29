d2a57a5ef17e0cc9bb64f321ff752d7ba64fcc45
AffanProg <affaninfobits@gmail.com>
2024-03-30 01:26:03 +0500

    added a new mwthod(way) i.e using while loop

--- program to reverse a number .py
+++ program to reverse a number .py
@@ -2,8 +2,8 @@
 
 num = int(input("Enter number :"))
 
-str_num = str(num)
-len_num = len(str_num)
+# str_num = str(num)
+# len_num = len(str_num)
 
 
 # 1st method i.e By Converting number into string
@@ -14,27 +14,36 @@ len_num = len(str_num)
 
 
 
-
+# Using for loop ( not the best way to do it)
 # 2nd method i.e Using list , then iterating it
 
-lst = []
+# =====================================
 
-for i in range(1,len_num+1):
-    last_digit = num%10
-    lst.append(last_digit)
+# lst = []
+
+# for i in range(1,len_num+1):
+#     last_digit = num%10
+#     lst.append(last_digit)
     
-    new_num = num//10
-    num = new_num
+#     new_num = num//10
+#     num = new_num
 
+# for reversed_num in lst:
+#     print(reversed_num, end='')
 
-# print("Reversed number:")
+# =====================================
 
-for reversed_num in lst:
-    print(reversed_num, end='')
+# 3rd method : using while loop (best way to do it)
 
+reverse = 0
 
+while(num!=0):
 
+    last = num%10
+    num = num//10
+    reverse = (reverse*10)+last
 
+print(reverse)
 
 
 
